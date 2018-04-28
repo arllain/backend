19d63d1709f54c9ddafa5b245b0bf51b1b935def
arllain <arllain@gmail.com>
2018-04-27 22:09:38 -0300

    Criação do endpoint /clientes/{id}

---
+++ src/main/java/com/arllain/backend/services/ClienteService.java
@@ -0,0 +1,23 @@
+package com.arllain.backend.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.arllain.backend.domain.Cliente;
+import com.arllain.backend.repositories.ClienteRepository;
+import com.arllain.backend.services.exception.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> cli = clienteRepository.findById(id);
+		return cli.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " +
+		id + ", Tipo: " + Cliente.class.getName()));
+	}
+}
