7cd00227dd80efcba07cf2e99847dec14264f50d
arllain <arllain@gmail.com>
2018-04-28 23:48:10 -0300

    Inserindo Categoria com o metodo POST

--- src/main/java/com/arllain/backend/controllers/CategoriaController.java
+++ src/main/java/com/arllain/backend/controllers/CategoriaController.java
@@ -1,11 +1,15 @@
 package com.arllain.backend.controllers;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.arllain.backend.domain.Categoria;
 import com.arllain.backend.services.CategoriaService;
@@ -22,5 +26,13 @@ public class CategoriaController {
 		Categoria categoria = service.buscar(id);
 		return ResponseEntity.ok(categoria);
 	}
-
+	
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria cat) {
+		cat = service.insert(cat);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(cat.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
 }
