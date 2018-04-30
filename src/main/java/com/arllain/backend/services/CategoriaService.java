f5edf44ca5f20800735b35c93ddc9cfe5160b10f
arllain <arllain@gmail.com>
2018-04-29 22:34:54 -0300

    Deletando uma Categoria

--- src/main/java/com/arllain/backend/services/CategoriaService.java
+++ src/main/java/com/arllain/backend/services/CategoriaService.java
@@ -3,22 +3,24 @@ package com.arllain.backend.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.arllain.backend.domain.Categoria;
 import com.arllain.backend.repositories.CategoriaRepository;
+import com.arllain.backend.services.exception.DataIntegrityException;
 import com.arllain.backend.services.exception.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repo;
-	
+
 	public Categoria find(Integer id) {
 		Optional<Categoria> cat = repo.findById(id);
-		return cat.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " +
-		id + ", Tipo: " + Categoria.class.getName()));
+		return cat.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 
 	public Categoria insert(Categoria cat) {
@@ -29,6 +31,15 @@ public class CategoriaService {
 	public Categoria update(Categoria cat) {
 		find(cat.getId());
 		return repo.save(cat);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos.");
+		}
 	}
 
 }
