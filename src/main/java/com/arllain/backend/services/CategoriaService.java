da205e2cbcbb90614ab7c055adc84b47f3c22183
arllain <arllain@gmail.com>
2018-05-01 07:45:35 -0300

    Paginacao Categoria

--- src/main/java/com/arllain/backend/services/CategoriaService.java
+++ src/main/java/com/arllain/backend/services/CategoriaService.java
@@ -5,9 +5,11 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
-import com.arllain.backend.controllers.dto.CategoriaDTO;
 import com.arllain.backend.domain.Categoria;
 import com.arllain.backend.repositories.CategoriaRepository;
 import com.arllain.backend.services.exception.DataIntegrityException;
@@ -47,5 +49,10 @@ public class CategoriaService {
 	public List<Categoria> findAll() {
 		return repo.findAll();
 	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}	
 
 }
