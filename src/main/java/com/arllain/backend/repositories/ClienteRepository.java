61d61dc312f117ebfd51a1cf82827e3ad9a568b9
arllain <arllain@gmail.com>
2018-05-07 22:51:09 -0300

    Validação de Inserção e Atualização de Email

--- src/main/java/com/arllain/backend/repositories/ClienteRepository.java
+++ src/main/java/com/arllain/backend/repositories/ClienteRepository.java
@@ -2,10 +2,13 @@ package com.arllain.backend.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.arllain.backend.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
 	
+	@Transactional(readOnly=true)
+	Cliente findByEmail(String email);
 }
--- src/main/java/com/arllain/backend/services/validation/ClienteUpdateValidator.java
+++ src/main/java/com/arllain/backend/services/validation/ClienteUpdateValidator.java
@@ -2,42 +2,42 @@ package com.arllain.backend.services.validation;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
 
 import com.arllain.backend.controllers.exception.FieldMessage;
 import com.arllain.backend.domain.Cliente;
-import com.arllain.backend.domain.enums.TipoCliente;
-import com.arllain.backend.dto.ClienteNewDTO;
+import com.arllain.backend.dto.ClienteDTO;
 import com.arllain.backend.repositories.ClienteRepository;
-import com.arllain.backend.services.validation.utils.BR;
 
-public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+public class ClienteUpdateValidator implements ConstraintValidator<ClienteUpdate, ClienteDTO> {
+	
+	@Autowired
+	private HttpServletRequest request;
 	
 	@Autowired
 	private ClienteRepository repo;
 	
 	@Override
-	public void initialize(ClienteInsert ann) {
+	public void initialize(ClienteUpdate ann) {
 	}
 
 	@Override
-	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+	public boolean isValid(ClienteDTO objDto, ConstraintValidatorContext context) {
+		
+		Map<String,String> map = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		Integer urlId = Integer.parseInt(map.get("id"));
+		
 		List<FieldMessage> list = new ArrayList<>();
 
-		if(objDto.getTipo().equals(TipoCliente.PESSOA_FISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
-			list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
-		}
-
-		if(objDto.getTipo().equals(TipoCliente.PESSOA_JURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
-			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
-		}
-		
 		Cliente cliente = repo.findByEmail(objDto.getEmail());
-		if (cliente != null) {
+		if (cliente != null && !cliente.getId().equals(urlId)) {
 			list.add(new FieldMessage("email", "Email já existente"));
 		}
 		
