31549f71cb7bb9bd4e9403c678e129187ae38019
arllain <arllain@gmail.com>
2018-05-13 23:30:26 -0300

    Inserindo pedidos

--- src/main/java/com/arllain/backend/controllers/PedidoController.java
+++ src/main/java/com/arllain/backend/controllers/PedidoController.java
@@ -1,13 +1,21 @@
 package com.arllain.backend.controllers;
 
+import java.net.URI;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.arllain.backend.domain.Categoria;
 import com.arllain.backend.domain.Pedido;
+import com.arllain.backend.dto.CategoriaDTO;
 import com.arllain.backend.services.PedidoService;
 
 @RestController
@@ -22,5 +30,13 @@ public class PedidoController {
 		Pedido ped = service.buscar(id);
 		return ResponseEntity.ok(ped);
 	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody Pedido obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}	
 
 }
