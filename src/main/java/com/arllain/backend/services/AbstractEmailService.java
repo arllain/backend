cc68bdd94ea37fa099c9ca79e44e4cbd7ad9f201
arllain <arllain@gmail.com>
2018-05-21 07:43:13 -0300

    Envio de email com template HTML

--- src/main/java/com/arllain/backend/services/AbstractEmailService.java
+++ src/main/java/com/arllain/backend/services/AbstractEmailService.java
@@ -2,15 +2,29 @@ package com.arllain.backend.services;
 
 import java.util.Date;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 import com.arllain.backend.domain.Pedido;
 
 public abstract class AbstractEmailService implements EmailService{
 
 	@Value("$(default.sender)")
-	private String sender; 
+	private String sender;
+	
+	@Autowired
+	TemplateEngine templateEngine;
+	
+	@Autowired
+	JavaMailSender javaMailSender;
 	
 	@Override
 	public void sendOrderConfirmationEmail(Pedido obj) {
@@ -26,5 +40,33 @@ public abstract class AbstractEmailService implements EmailService{
 		sn.setSentDate(new Date(System.currentTimeMillis()));
 		sn.setText(obj.toString());
 		return sn;
+	}
+	
+	
+	protected String htmlFromTemplatePedido(Pedido obj) {
+		Context context = new Context();
+		context.setVariable("pedido", obj);
+		return templateEngine.process("email/confirmacaoPedido", context);
+	}
+	
+	@Override
+	public void sendOrderConfirmationHtmlEmail(Pedido obj) {
+		MimeMessage mm;
+		try {
+			mm = prepareMimeMessagePedido(obj);
+			sendHtmlEmail(mm);
+		} catch (MessagingException e) {
+			sendOrderConfirmationEmail(obj);
+		}
+	}
+
+	protected MimeMessage prepareMimeMessagePedido(Pedido obj) throws MessagingException {
+		MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+		MimeMessageHelper mmh = new MimeMessageHelper(mimeMessage, true);
+		mmh.setTo(obj.getCliente().getEmail());
+		mmh.setFrom(sender);
+		mmh.setSubject("Pedido confirmado! Código: " + obj.getId());
+		mmh.setText(htmlFromTemplatePedido(obj),true);
+		return mimeMessage;
 	}
 }
