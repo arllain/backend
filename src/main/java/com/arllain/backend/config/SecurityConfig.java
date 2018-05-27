09b48e0415c46f68fed03effc023e1af011e33d2
arllain <arllain@gmail.com>
2018-05-27 12:59:09 -0300

    Autenticacao e geração de token JWT

--- src/main/java/com/arllain/backend/config/SecurityConfig.java
+++ src/main/java/com/arllain/backend/config/SecurityConfig.java
@@ -7,13 +7,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.arllain.backend.security.JWTAuthenticationFilter;
+import com.arllain.backend.security.JWTUtil;
 
 @Configuration
 @EnableWebSecurity
@@ -21,6 +27,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	@Autowired
 	private Environment env;
+	
+	@Autowired
+	private UserDetailsService userDetailsService;
+	
+	@Autowired
+	private JWTUtil jwtUtil;
 	
 	private static final String[] PUBLIC_MATCHERS = {
 			"/h2-console/**"
@@ -40,13 +52,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 			http.headers().frameOptions().disable();
 		}
 		
-		http.cors();
+		http.cors().and().csrf().disable();
 		http.authorizeRequests()
-		.antMatchers(PUBLIC_MATCHERS).permitAll()
 		.antMatchers(HttpMethod.GET,PUBLIC_MATCHERS_GET).permitAll()
+		.antMatchers(PUBLIC_MATCHERS).permitAll()
 		.anyRequest().authenticated();
-		
+
+		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
+	}	
 	
 	@Bean
 	CorsConfigurationSource configurationSource() {
