c56a802e0e2805b2a221c4c95bd3742f91db7cfb
arllain <arllain@gmail.com>
2018-06-22 02:45:17 -0300

    Salvando arquivos no S3 do Amazon Web Service

--- src/main/java/com/arllain/backend/BackendApplication.java
+++ src/main/java/com/arllain/backend/BackendApplication.java
@@ -1,12 +1,17 @@
 package com.arllain.backend;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.arllain.backend.services.S3Service;
+
 @SpringBootApplication
 public class BackendApplication implements CommandLineRunner{
 
+	@Autowired
+	S3Service s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
@@ -14,6 +19,7 @@ public class BackendApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
+		s3Service.uploadFile("C:\\00\\coffee.jpg");
 
 	}
 }
